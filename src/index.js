29d4b00f799be97fb009d3c89c3530d2893391d2
AliVik <alvikul1994@gmail.com>
2021-11-10 22:19:15 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './css/styles.css';
 import { fetchCountries} from './js/fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
-console.log(fetchCountries())
+console.log(fetchCountries('ukraine'))
 
 
 
