e5e8a0e02ac4d697ebaf9589d62fee3d78f0637d
AliVik <alvikul1994@gmail.com>
2021-11-10 22:14:48 +0200

    импортирует функцию сбора данных

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import './css/styles.css';
+import { fetchCountries} from './js/fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
+console.log(fetchCountries())
+
+
+
+// GET https://restcountries.com/v3.1/name/{name}
+// Accept: application/json
