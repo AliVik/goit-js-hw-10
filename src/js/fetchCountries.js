d96d647327f08fd9db2b32332f8c193544d1ab39
AliVik <alvikul1994@gmail.com>
2021-11-12 21:09:31 +0200

    импортирует библиотеку уведомлений, добавляет уведомления результатов ответа с бэкенда

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,15 +1,33 @@
-export function fetchCountries (name) {
-    return fetch(`https://restcountries.com/v3.1/name/${name}`)
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+
+export function fetchCountries(name) {
+    return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
         .then(response => {
             if (!response.ok) {
                 throw new Error(response.status);
             }
             return response.json();
         })
-        .then(result => console.log(result))
-        .catch(error=>console.log(error))
+        .then(result => {
+            if (result.length > 10) {
+                Notify.info('Too many matches found. Please enter a more specific name.')
+            }
+        })
+        .catch(() => Notify.failure("Oops, there is no country with that name"));
+}
+    
+    // function checkServerResponse(result) {
+    //      if (result.length > 10) {
+    //             Notify.info('Too many matches found. Please enter a more specific name.');
+    //         } else if (result.length >= 2 && result.length <= 10) {
+    //             // create markup with all countries
+    //         } else if (result.length === 1) {
+    //             // create markup of country with all its properties
+    //         }
+    // }
      
     
-}
+
 
 
