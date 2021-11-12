fd73e298ca9c8c0601be6f86025aa013255e25d5
AliVik <alvikul1994@gmail.com>
2021-11-12 21:10:59 +0200

    добавляет функцию получения значения с инпута, набросок функции создания разметки

--- src/index.js
+++ src/index.js
@@ -1,7 +1,29 @@
 import './css/styles.css';
-import { fetchCountries} from './js/fetchCountries';
+import { fetchCountries } from './js/fetchCountries';
+import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
-console.log(fetchCountries('ukraine'))
 
+const refs = {
+    input: document.querySelector('input#search-box'),
+}
+
+
+refs.input.addEventListener('input', debounce(onInputFunc, DEBOUNCE_DELAY));
+
+
+
+
+function onInputFunc() {
+    const inputValue = refs.input.value.trim();
+    console.log(inputValue);
+}
+
+
+// function createCountryList(countries) {
+//     const markup = countries.map(({ name, flag }) => {
+//         `<li>${flag.svg}${name.official}</li>`
+//     }).join('')
+    
+// }
 
